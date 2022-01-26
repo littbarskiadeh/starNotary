e559f4b1a726a9b498dd5bd4798ea3a04c61bb46
littbarskiadeh <lit.adeh@gmail.com>
2022-01-26 08:51:01 -0500

    deployed to rinkeby testnet

---
+++ README.md
@@ -0,0 +1,13 @@
+Truffle v5.3.11 
+Solidity Compiler Version: 0.8.4
+
+Openzeppelin Contracts - 4.4.2
+Truffle Hdwallet-provider - 2.0.1
+
+Token Name - Bigheadphones
+Token Symbol - BHP
+
+
+Rinkeby Address - 0x6983F76Efe6c0C2CD9A09050e1Ad657F07295616
+
+Testnet Deployment transaction details - https://rinkeby.etherscan.io/tx/0x3d9a6f7f85ab3728327544f5e7e14a8a83f4dd3d70c5ed9f0de0074579d9dd61
--- truffle-config.js
+++ truffle-config.js
@@ -21,12 +21,12 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-
-// const HDWallet = require('truffle-hdwallet-provider');
+ require('dotenv').config() 
+const HDWallet = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -51,7 +51,12 @@ module.exports = {
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/f97044adaa2a41a8abd9ec24fa5d80e6`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    },
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
